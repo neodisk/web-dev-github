487e9b7a0d3f1d58ef84bd4781070597a5be7c14
Jason Lee <neodisk@gmail.com>
2026-01-21 15:00:36 +0900

    Update main.py

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"status": "Cloud Run is working!"}
+    return {"status": "Alive", "message": "Cloud Run is working!"}
 
-# 서버 실행 (이 부분은 Docker가 CMD로 실행하지만, 로컬 테스트용으로 남겨둡니다)
+# Docker가 실행할 때 이 부분이 호출됩니다.
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8080)
